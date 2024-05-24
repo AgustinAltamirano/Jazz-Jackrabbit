503b1e31bd60ab9b85cbd36e4d819b5635999cff
FedeGen12 <fedegen14@gmail.com>
2024-05-24 07:46:25 -0300

    Borro el botón del modo editor en el MenuPrincipal

--- client/vista_menu/menu_principal.cpp
+++ client/vista_menu/menu_principal.cpp
@@ -6,8 +6,7 @@
 MenuPrincipal::MenuPrincipal(QFont &fuente) :
         QMainWindow(),
         new_game_btn(this, POS_NEWGAME_BTN, WIDTH_NEWGAME_BTN, TEXT_NEWGAME_BTN, fuente),
-        join_game_btn(this, POS_JOINGAME_BTN, WIDTH_JOINGAME_BTN, TEXT_JOINGAME_BTN, fuente),
-        editor_mode_btn(this, POS_EDITOR_BTN, WIDTH_EDITOR_BTN, TEXT_EDITOR_BTN, fuente)
+        join_game_btn(this, POS_JOINGAME_BTN, WIDTH_JOINGAME_BTN, TEXT_JOINGAME_BTN, fuente)
 {
     setFixedSize(ANCHO_PANTALLA, ALTO_PANTALLA);
     setStyleSheet("QMainWindow {background-image: url(:/img/img/bgd_mainmenu.jpg);}");
@@ -19,7 +18,6 @@ void MenuPrincipal::conectar_botones() {
     // Defino los handlers de los botones
     connect(&new_game_btn, &QPushButton::clicked,this,&MenuPrincipal::mostrar_menu_crear_partida);
     connect(&join_game_btn, &QPushButton::clicked,this,&MenuPrincipal::mostrar_menu_unirse_partida);
-    connect(&editor_mode_btn, &QPushButton::clicked,this,&MenuPrincipal::mostrar_menu_editor);
 }
 
 
@@ -33,12 +31,5 @@ void MenuPrincipal::mostrar_menu_crear_partida() {
 void MenuPrincipal::mostrar_menu_unirse_partida() {
     // menu_unirse_partida = new MenuUnirsePartida(this, fuente_juego);
     // menu_unirse_partida->show();
-    hide();
-}
-
-
-void MenuPrincipal::mostrar_menu_editor() {
-    // menu_editor_partida = new MenuEditorPartida(this, fuente_juego);
-    // menu_editor_partida->show();
     hide();
 }
--- client/vista_menu/menu_principal.h
+++ client/vista_menu/menu_principal.h
@@ -9,16 +9,12 @@
 enum PosicionBoton {
     POS_NEWGAME_BTN,
     POS_JOINGAME_BTN,
-    POS_EDITOR_BTN
 };
 
 #define WIDTH_NEWGAME_BTN 290
 #define TEXT_NEWGAME_BTN "NEW GAME"
 #define WIDTH_JOINGAME_BTN 321
 #define TEXT_JOINGAME_BTN "JOIN GAME"
-#define WIDTH_EDITOR_BTN 391
-#define TEXT_EDITOR_BTN "EDITOR MODE"
-
 
 class VentanaInicial;  // Declaración adelantada
 
@@ -28,15 +24,11 @@ private:
 
     BotonMenu join_game_btn;
 
-    BotonMenu editor_mode_btn;
-
     void conectar_botones();
 
     void mostrar_menu_crear_partida();
 
     void mostrar_menu_unirse_partida();
-
-    void mostrar_menu_editor();
 
 public:
     explicit MenuPrincipal(QFont &fuente);
