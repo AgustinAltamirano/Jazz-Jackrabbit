9c85756f0a2e19d7ca528b8a6f72bf32333e8fff
SantiagoFassio <sfassio@fi.uba.ar>
2024-05-30 19:39:30 -0300

    Cambios hechos para ajustar los cambios pedidos: cliente.cpp -> #include <utility> servidor_deserializador -> caso default para cuando el switch no tiene los cambios esperados lo mismo para cliente_deserializador monitor_partidas -> acumulate en vez de loop comando_unir_dto -> inicializo parametros no inicializados como genericos lo mismo con comando_crear_dto monitorpartidas -> uso de int32 y uint32 gestor_partidas.h -> borrar_partidas_finalizadas() ahora es un metodo publico constructor de gameloop creado

--- src/server/logica_juego/assets/spawnpoint.h
+++ src/server/logica_juego/assets/spawnpoint.h
@@ -1,11 +1,13 @@
 #ifndef SPAWNPOINT_H
 #define SPAWNPOINT_H
 
+#include <cstdint>
+
 class spawnpoint {
 public:
-    int pos_x;
-    int pos_y;
-    spawnpoint(const int x, const int y) {
+    int32_t pos_x;
+    int32_t pos_y;
+    spawnpoint(const int32_t x, const int32_t y) {
         pos_x = x;
         pos_y = y;
     }
