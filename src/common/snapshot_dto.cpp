f00a260ac03582cddad41207fdcc1baa8029d667
Netanel David Jamilis <23279713+netaneldj@users.noreply.github.com>
2024-06-04 18:42:47 -0300

    agrego atributos a snapshot dto

--- src/common/snapshot_dto.cpp
+++ src/common/snapshot_dto.cpp
@@ -2,14 +2,29 @@
 
 #include <iostream>
 
-SnapshotDTO::SnapshotDTO(): clientes(), fin_juego(false) {}
+SnapshotDTO::SnapshotDTO(): clientes(), bloques_escenario(), tipo_escenario(), fin_juego(false) {}
 
-SnapshotDTO::SnapshotDTO(const std::vector<ClienteDTO> clientes):
-        clientes(clientes), fin_juego(false) {}
+SnapshotDTO::SnapshotDTO(TipoEscenario tipo_escenario):
+        clientes(), bloques_escenario(), tipo_escenario(tipo_escenario), fin_juego(false) {}
+
+SnapshotDTO::SnapshotDTO(std::vector<ClienteDTO> clientes, std::vector<BloqueEscenarioDTO> bloques,
+                         TipoEscenario tipo_escenario):
+        clientes(clientes),
+        bloques_escenario(bloques),
+        tipo_escenario(tipo_escenario),
+        fin_juego(false) {}
+
+void SnapshotDTO::establecer_tipo_escenario(TipoEscenario tipo_escenario) {
+    this.tipo_escenario = tipo_escenario;
+}
 
 void SnapshotDTO::agregar_cliente(ClienteDTO cliente) { clientes.push_back(cliente); }
 
 std::vector<ClienteDTO> SnapshotDTO::obtener_clientes() { return clientes; }
+
+void SnapshotDTO::agregar_bloque_escenario(BloqueEscenarioDTO bloque_escenario) { bloques_escenario.push_back(bloque_escenario); }
+
+std::vector<BloqueEscenarioDTO> SnapshotDTO::obtener_bloques_escenario() { return bloques_escenario; }
 
 bool SnapshotDTO::es_fin_juego() const { return fin_juego; }
 
--- src/common/snapshot_dto.h
+++ src/common/snapshot_dto.h
@@ -4,28 +4,75 @@
 #include <cstdint>
 #include <vector>
 
+#include "../client/vista_juego/snapshot_dto.h"
+
+#include "estado_personaje.h"
+#include "tipo_personaje.h"
+
 struct ClienteDTO {
     int32_t id_cliente;
+    TipoPersonaje tipo_personaje;
+    int32_t pos_x, pos_y;
+    bool de_espaldas;
+    EstadoPersonaje estado;
+    uint32_t vida;
+    uint32_t puntos;
+    ArmaActual arma_actual;
+    int32_t balas_restantes;
 
-    explicit ClienteDTO(int32_t id_cliente): id_cliente(id_cliente) {}
+    explicit ClienteDTO(iconst uint32_t id_cliente, const TipoPersonaje tipo_personaje,
+                        const int32_t pos_x, const int32_t pos_y, const bool de_espaldas,
+                        const EstadoPersonaje estado, const uint32_t vida, const uint32_t puntos,
+                        const ArmaActual arma_actual, const int32_t balas_restantes):
+            id_cliente(id_cliente),
+            tipo_personaje(tipo_personaje),
+            pos_x(pos_x),
+            pos_y(pos_y),
+            de_espaldas(de_espaldas),
+            estado(estado),
+            vida(vida),
+            puntos(puntos),
+            arma_actual(arma_actual),
+            balas_restantes(balas_restantes) {}
+};
+
+struct BloqueEscenarioDTO {
+    int32_t pos_x, pos_y, angulo;
+    uint32_t ancho, alto;
+    TipoBloqueEscenario tipo;
+    BloqueEscenarioDTO(int32_t pos_x, int32_t pos_y, uint32_t ancho, uint32_t alto, int32_t angulo,
+                       TipoEscenario tipo):
+            pos_x(pos_x), pos_y(pos_y), ancho(ancho), alto(alto), angulo(angullo), tipo(tipo);
 };
 
 class SnapshotDTO {
 private:
+    TipoEscenario tipo_escenario;
+    std::vector<BloqueEscenarioDTO> bloques_escenario;
     std::vector<ClienteDTO> clientes;
-
-    bool fin_juego{};
+    bool fin_juego;
 
 public:
     SnapshotDTO();
 
-    explicit SnapshotDTO(std::vector<ClienteDTO> clientes);
+    SnapshotDTO(TipoEscenario tipo_escenario);
+
+    SnapshotDTO(std::vector<ClienteDTO> clientes, std::vector<BloqueEscenarioDTO> bloques,
+                TipoEscenario tipo_escenario);
+
+    void establecer_tipo_escenario(TipoEscenario tipo_escenario);
+
+    TipoEscenario obtener_tipo_escenario();
 
     void agregar_cliente(ClienteDTO cliente);
 
     std::vector<ClienteDTO> obtener_clientes();
 
-    [[nodiscard]] bool es_fin_juego() const;
+    void agregar_bloque_escenario(BloqueEscenarioDTO bloque_escenario);
+
+    std::vector<BloqueEscenarioDTO> obtener_bloques_escenario();
+
+    bool es_fin_juego();
 
     void establecer_fin_juego(bool fin_juego);
 };
