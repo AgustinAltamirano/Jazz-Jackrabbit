860fdd28151659cf3204474100188d01195d6160
FedeGen12 <fedegen14@gmail.com>
2024-06-11 12:11:52 -0300

    Corrección en el CMakeLists raíz y en el de la vista_juego + eliminación de clase en desuso

--- src/common/comando_comenzar_dto.cpp
+++
@@ -1,7 +0,0 @@
-#include "comando_comenzar_dto.h"
-
-ComandoComenzarDTO::ComandoComenzarDTO(): ComandoDTO(COMENZAR), empezo(false) {}
-
-ComandoComenzarDTO::ComandoComenzarDTO(bool unio): ComandoDTO(COMENZAR), empezo(unio) {}
-
-bool ComandoComenzarDTO::obtener_empezo() { return empezo; }
