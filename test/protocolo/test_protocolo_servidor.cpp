265b3bb0ef0dafe442a25f5d5d82d3ad80963f6d
Netanel David Jamilis <23279713+netaneldj@users.noreply.github.com>
2024-06-21 17:43:40 -0300

    agrego tests protocolo servidor crear partida

--- test/protocolo/test_protocolo_servidor.cpp
+++ test/protocolo/test_protocolo_servidor.cpp
@@ -78,9 +78,48 @@ void test_enviar_unir_partida(void) {
     TEST_CHECK(unio_partida_recibido == unio_partida_enviado);
 }
 
+void test_enviar_crear_partida(void) {
+    SocketDummy socket;
+    ServidorProtocolo protocolo(&socket);
+    bool cerrado = false;
+    int32_t codigo_partida_enviado = 2;
+
+    protocolo.enviar_crear_partida(codigo_partida_enviado, &cerrado);
+
+    char tipo_comando_recibido;
+    socket.recvall(&tipo_comando_recibido, 1, &cerrado);
+
+    int32_t codigo_partida_recibido;
+    socket.recvall(&codigo_partida_recibido, 4, &cerrado);
+    codigo_partida_recibido = ntohl(codigo_partida_recibido);
+
+    TEST_CHECK(CREAR == tipo_comando_recibido);
+    TEST_CHECK(codigo_partida_recibido == codigo_partida_enviado);
+}
+
+void test_enviar_error_crear_partida(void) {
+    SocketDummy socket;
+    ServidorProtocolo protocolo(&socket);
+    bool cerrado = false;
+
+    protocolo.enviar_error_crear_partida(&cerrado);
+
+    char tipo_comando_recibido;
+    socket.recvall(&tipo_comando_recibido, 1, &cerrado);
+
+    int32_t codigo_partida_recibido;
+    socket.recvall(&codigo_partida_recibido, 4, &cerrado);
+    codigo_partida_recibido = ntohl(codigo_partida_recibido);
+
+    TEST_CHECK(CREAR == tipo_comando_recibido);
+    TEST_CHECK(codigo_partida_recibido == -1);
+}
+
 
 TEST_LIST = {{"Test protocolo obtener comando", test_obtener_comando},
              {"Test protocolo enviar id cliente", test_enviar_id_cliente},
              {"Test protocolo enviar validar escenario", test_enviar_validar_escenario},
              {"Test protocolo enviar unir partida", test_enviar_unir_partida},
+             {"Test protocolo enviar crear partida", test_enviar_crear_partida},
+             {"Test protocolo enviar error crear partida", test_enviar_error_crear_partida},
              {NULL, NULL}};
