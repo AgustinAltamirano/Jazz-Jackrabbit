886d17be7e18aecdb6238fa4975b563eae55ed24
Netanel David Jamilis <23279713+netaneldj@users.noreply.github.com>
2024-06-22 19:18:51 -0300

    utilizo los getters del snapshot

--- test/protocolo/test_protocolo_cliente.cpp
+++ test/protocolo/test_protocolo_cliente.cpp
@@ -31,72 +31,72 @@ void test_obtener_snapshot(void) {
     TEST_CHECK(snapshot_recibido->obtener_tipo_escenario() ==
                snapshot_enviado->obtener_tipo_escenario());
 
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->pos_x ==
-               snapshot_enviado->obtener_bloques_escenario().data()->pos_x);
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->pos_y ==
-               snapshot_enviado->obtener_bloques_escenario().data()->pos_y);
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->angulo ==
-               snapshot_enviado->obtener_bloques_escenario().data()->angulo);
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->alto ==
-               snapshot_enviado->obtener_bloques_escenario().data()->alto);
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->ancho ==
-               snapshot_enviado->obtener_bloques_escenario().data()->ancho);
-    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->tipo ==
-               snapshot_enviado->obtener_bloques_escenario().data()->tipo);
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_pos_x() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_pos_x());
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_pos_y() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_pos_y());
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_angulo() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_angulo());
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_alto() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_alto());
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_ancho() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_ancho());
+    TEST_CHECK(snapshot_recibido->obtener_bloques_escenario().data()->obtener_tipo() ==
+               snapshot_enviado->obtener_bloques_escenario().data()->obtener_tipo());
 
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->id_cliente ==
-               snapshot_enviado->obtener_clientes().data()->id_cliente);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->tipo_personaje ==
-               snapshot_enviado->obtener_clientes().data()->tipo_personaje);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->pos_x ==
-               snapshot_enviado->obtener_clientes().data()->pos_x);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->pos_y ==
-               snapshot_enviado->obtener_clientes().data()->pos_y);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->de_espaldas ==
-               snapshot_enviado->obtener_clientes().data()->de_espaldas);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->estado ==
-               snapshot_enviado->obtener_clientes().data()->estado);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->vida ==
-               snapshot_enviado->obtener_clientes().data()->vida);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->puntos ==
-               snapshot_enviado->obtener_clientes().data()->puntos);
-    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->balas_restantes ==
-               snapshot_enviado->obtener_clientes().data()->balas_restantes);
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_id_cliente() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_id_cliente());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_tipo_personaje() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_tipo_personaje());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_pos_x() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_pos_x());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_pos_y() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_pos_y());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_de_espaldas() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_de_espaldas());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_estado() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_estado());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_vida() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_vida());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_puntos() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_puntos());
+    TEST_CHECK(snapshot_recibido->obtener_clientes().data()->obtener_balas_restantes() ==
+               snapshot_enviado->obtener_clientes().data()->obtener_balas_restantes());
 
-    TEST_CHECK(snapshot_recibido->obtener_balas().data()->pos_x ==
-               snapshot_enviado->obtener_balas().data()->pos_x);
-    TEST_CHECK(snapshot_recibido->obtener_balas().data()->pos_y ==
-               snapshot_enviado->obtener_balas().data()->pos_y);
-    TEST_CHECK(snapshot_recibido->obtener_balas().data()->tipo ==
-               snapshot_enviado->obtener_balas().data()->tipo);
-    TEST_CHECK(snapshot_recibido->obtener_balas().data()->choco ==
-               snapshot_enviado->obtener_balas().data()->choco);
+    TEST_CHECK(snapshot_recibido->obtener_balas().data()->obtener_pos_x() ==
+               snapshot_enviado->obtener_balas().data()->obtener_pos_x());
+    TEST_CHECK(snapshot_recibido->obtener_balas().data()->obtener_pos_y() ==
+               snapshot_enviado->obtener_balas().data()->obtener_pos_y());
+    TEST_CHECK(snapshot_recibido->obtener_balas().data()->obtener_pos_y() ==
+               snapshot_enviado->obtener_balas().data()->obtener_tipo());
+    TEST_CHECK(snapshot_recibido->obtener_balas().data()->obtener_choco() ==
+               snapshot_enviado->obtener_balas().data()->obtener_choco());
 
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->id ==
-               snapshot_enviado->obtener_enemigos().data()->id);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->pos_x ==
-    snapshot_enviado->obtener_enemigos().data()->pos_x);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->pos_y ==
-    snapshot_enviado->obtener_enemigos().data()->pos_y);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->alto ==
-    snapshot_enviado->obtener_enemigos().data()->alto);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->ancho ==
-               snapshot_enviado->obtener_enemigos().data()->ancho);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->tipo ==
-               snapshot_enviado->obtener_enemigos().data()->tipo);
-    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->invertido ==
-               snapshot_enviado->obtener_enemigos().data()->invertido);
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_id() ==
+               snapshot_enviado->obtener_enemigos().data()->obtener_id());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_pos_x() ==
+    snapshot_enviado->obtener_enemigos().data()->obtener_pos_x());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_pos_y() ==
+    snapshot_enviado->obtener_enemigos().data()->obtener_pos_y());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_alto() ==
+    snapshot_enviado->obtener_enemigos().data()->obtener_alto());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_ancho() ==
+               snapshot_enviado->obtener_enemigos().data()->obtener_ancho());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_tipo() ==
+               snapshot_enviado->obtener_enemigos().data()->obtener_tipo());
+    TEST_CHECK(snapshot_recibido->obtener_enemigos().data()->obtener_invertido() ==
+               snapshot_enviado->obtener_enemigos().data()->obtener_invertido());
 
-    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->pos_x ==
-               snapshot_enviado->obtener_recogibles().data()->pos_x);
-    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->pos_y ==
-               snapshot_enviado->obtener_recogibles().data()->pos_y);
-    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->alto ==
-               snapshot_enviado->obtener_recogibles().data()->alto);
-    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->ancho ==
-               snapshot_enviado->obtener_recogibles().data()->ancho);
-    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->tipo ==
-               snapshot_enviado->obtener_recogibles().data()->tipo);
+    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->obtener_pos_x() ==
+               snapshot_enviado->obtener_recogibles().data()->obtener_pos_x());
+    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->obtener_pos_y() ==
+               snapshot_enviado->obtener_recogibles().data()->obtener_pos_y());
+    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->obtener_alto() ==
+               snapshot_enviado->obtener_recogibles().data()->obtener_alto());
+    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->obtener_ancho() ==
+               snapshot_enviado->obtener_recogibles().data()->obtener_ancho());
+    TEST_CHECK(snapshot_recibido->obtener_recogibles().data()->obtener_tipo() ==
+               snapshot_enviado->obtener_recogibles().data()->obtener_tipo());
 
     TEST_CHECK(snapshot_recibido->obtener_tiempo_restante() ==
                snapshot_enviado->obtener_tiempo_restante());
